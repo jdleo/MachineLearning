6bee919baa8a26661d5f722c2e94e92216787d67
John Leonardo <jdleonardo5@icloud.com>
2016-11-27 17:59:54 -0500

    Update stock-prediction.py

--- stock-prediction.py
+++ stock-prediction.py
@@ -1,3 +1,8 @@
+'''
+Pandas for organizing our datasets, Quandl because it is has the best collection of data sets imo,
+math for obvious reasons, datetime/time for handling timestamps, sklearn for machine learning,
+matplotlib for plotting our data on a nice graph, and style for, well, style ;)
+'''
 import pandas as pd
 import quandl, math, datetime, time
 import numpy as np
