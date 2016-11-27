d44d8345f0a671017615221c385d7ea56a616c9b
John Leonardo <jdleonardo5@icloud.com>
2016-11-27 18:27:22 -0500

    done, just need to fix some styling shit

--- stock-prediction.py
+++ stock-prediction.py
@@ -62,4 +62,25 @@ clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
 print("Confidence score for this stock prediction: {}".format(confidence))
 
+forecast_set = clf.predict(X_lately)
+#print(forecast_set)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.to_datetime()
+one_day = 86400
+next_unix = time.mktime(last_unix.timetuple()) + one_day
+
+for i in forecast_set:
+  next_date = datetime.datetime.fromtimestamp(next_unix)
+  next_unix += one_day
+  df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
 
