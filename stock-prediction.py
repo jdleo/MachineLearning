63a661dc8a4b77986656201a037cf0ac71af01ee
John Leonardo <jdleonardo5@icloud.com>
2016-11-27 18:03:29 -0500

    get quandl

--- stock-prediction.py
+++ stock-prediction.py
@@ -10,3 +10,14 @@ from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+
+#user enters stock symbol, and we format it for quandl
+stockSymbol = input("Enter a stock symbol you want to predict for (Ex: AAPL): ")
+qCode = 'WIKI/{}'.format(stockSymbol)
+
+df = qandl.get(qCode)
+
+#keep relevant columns for testing
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+
+
