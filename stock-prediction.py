6450d11c6343dd283cd3eab56d741afd15e1997a
John Leonardo <jdleonardo5@icloud.com>
2016-11-27 17:58:16 -0500

    libraries

---
+++ stock-prediction.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import quandl, math, datetime, time
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
