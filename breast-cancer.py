87583a2ea1db958a4b489c003013f2b3d756dde8
John Leonardo <jdleonardo5@icloud.com>
2016-11-27 19:10:21 -0500

    done

--- breast-cancer.py
+++ breast-cancer.py
@@ -19,3 +19,34 @@ y = np.array(df['class'])
 #train/test
 x_train, x_test, y_train, y_test = cross_validation.train_test_split(x,y,test_size=0.2)
 
+clf = neighbors.KNeighborsClassifier()
+clf.fit(x_train,y_train)
+
+confidence = clf.score(x_test,y_test)
+#print("Confidence score: {}".format(confidence))
+
+print("Enter your patients tumor classifications")
+print("Score 1-10. Clump thickness, Uniformity of Cell Size, Uniformity of Cell Shape,  Marginal Adhesion,")
+print("Single Epithelial Cell Size, Bare Nuclei count, Bland Chromatin count, Normal Nucleoli count, Mitoses")
+
+#user input
+values = str(raw_input("Enter 1-10 for each above back-to-back. Example 123221234:\n"))
+
+arr = []
+
+for i in values:
+  arr.append(int(i))
+
+prediction_measures = np.array(arr)
+prediction_measures = prediction_measures.reshape(1,-1)
+
+prediction = clf.predict(prediction_measures)
+
+#decision
+if prediction == [2]:
+	print("The tumor is benign with a confidence score of {}".format(confidence))
+elif prediction == [4]:
+	print("The tumor is malignant with a confidence score of {}".format(confidence))
+
+
+
